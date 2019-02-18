eb86b7be78fab95e4a482e7f3846992fe9b79b27
Alan <hernandezalan299@gmail.com>
2019-02-18 14:42:31 -0800

    Rearranged buttons and planned out infrastruture for entire grid space transformations.

--- View.py
+++ View.py
@@ -10,7 +10,6 @@ class View:
     def create_grid(self, event=None):
         w = self.c.winfo_width()
         h = self.c.winfo_height()
-        self.c.delete('grid_line')
 
         #Creates all vericle lines at intervals of 100
         for i in range(0,w,20):
@@ -24,17 +23,17 @@ class View:
         self.c.create_line([(260,0),(260,500)],tag='x-axis',fill='blue')
         #Create the y-axis
         self.c.create_line([(0,260),(500,260)],tag='y-axis',fill='blue')
-
+    
     def drawVector(self, vec):
-        self.vector1 = self.c.create_line([260,260], vec.getGUIVector1(), tag='first')
-        self.vector2 = self.c.create_line([260,260], vec.getGUIVector2(), tag='second')
+        vector1 = self.c.create_line([260,260], vec.getGUIVector1(), tag='first')
+        vector2 = self.c.create_line([260,260], vec.getGUIVector2(), tag='second')
         self.root.update_idletasks()
         self.root.update()
 
     def unDrawVector(self):
         if(self.hasBeenDrawnOn):
-            self.c.delete(self.vector1)
-            self.c.delete(self.vector2)
+            self.c.delete('first')
+            self.c.delete('second')
         
     def waitForClick(self):
         self.root.update_idletasks()
@@ -79,13 +78,18 @@ class View:
         #Input labels and textfields
         Label(inputFrame, text="X coordinate").grid(row=0)
         Label(inputFrame, text="Y coordinate").grid(row=1)
-        Label(inputFrame, text="Rotate by").grid(row=0, column=2)
-        self.e1 = Entry(inputFrame)
+        Label(inputFrame, text="Rotate by").grid(row=0, column=4)
+        Label(inputFrame, text="X1").grid(row=0,column=2)
+        Label(inputFrame, text="Y1").grid(row=1,column=2)
+        self.e1 = Entry(inputFrame, width=1)
         self.e1.grid(row=0, column=1)
-        self.e2 = Entry(inputFrame)
+        self.e2 = Entry(inputFrame, width=1)
         self.e2.grid(row=1, column=1)
-        self.e3 = Entry(inputFrame)
-        self.e3.grid(row=0, column=3)
+        self.e3 = Entry(inputFrame, width=3)
+        self.e3.grid(row=0, column=5)
+        #Matrix input entries
+        self.e4 = Entry(inputFrame, width=1)
+        self.e4.grid(row=0,column=3)
 
         #For letting class methods know that this is a fresh canvas
         self.hasBeenDrawnOn = False
